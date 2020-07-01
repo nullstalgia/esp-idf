c7ca88081e860bde081bfa920fa0a04c56fb9793
nullstalgia <nullstalgia@gmail.com>
2020-07-01 00:46:44 -0700

    Update add_path.sh

--- add_path.sh
+++ add_path.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # This shell snippet appends useful esp-idf tools to your PATH environment
 # variable. This means you can run esp-idf tools without needing to give the
 # full path.
@@ -6,16 +7,18 @@
 #
 # . ${IDF_PATH}/add_path.sh
 #
-if [ -z ${IDF_PATH} ]; then
-	echo "IDF_PATH must be set before including this script."
-else
-	IDF_ADD_PATHS_EXTRAS=
-	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/esptool_py/esptool"
-	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/espcoredump"
-	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/partition_table/"
-	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/tools/"
-	export PATH="${IDF_ADD_PATHS_EXTRAS}:${PATH}"
-	echo "Added to PATH: ${IDF_ADD_PATHS_EXTRAS}"
-fi
+#if [ -z ${IDF_PATH} ]; then
+#	echo "IDF_PATH must be set before including this script."
+#else
+IDF_PATH="/home/tony/esp/"
+IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}"
+IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/esptool_py/esptool"
+IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/espcoredump"
+IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/partition_table/"
+IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/tools/"
+IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/xtensa-esp32-elf/bin/"
+#PATH="${IDF_ADD_PATHS_EXTRAS}:${PATH}"
+echo "export PATH=\"\$PATH${IDF_ADD_PATHS_EXTRAS}\";export IDF_PATH=\"${IDF_PATH}\""
+#fi
 
 
